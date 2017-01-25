4128240f30156b3f0c1d86f1a07139a94592d776
packerfan626 <dori_mouawad@hotmail.com>
2017-01-24 16:18:17 -0800

    Login and Register

--- CelebritiesSquared/LoginScreen.swift
+++ CelebritiesSquared/LoginScreen.swift
@@ -29,8 +29,43 @@ class LoginScreen: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func postToServerFunction(){
+        //Contingency Handling. Error handling etc.
+        var username : String = usernameField.text!
+        var password : String = passwordField.text!
+        
+        
+        var request = URLRequest(url: URL(string: "http://celebritiessquared.com/CSPhp/SignIn.php")!)
+        
+        request.httpMethod = "POST"
+        
+        var postString: String!
+        postString = "username=\(username)&password=\(password)"
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            
+        }
+        task.resume()
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
         //check with database and change segue to be done programattically so that it confirms the login before going.
+        
+        postToServerFunction()
+        
     }
     
     
--- CelebritiesSquared/RegisterScreen.swift
+++ CelebritiesSquared/RegisterScreen.swift
@@ -11,6 +11,10 @@ import UIKit
 
 class RegisterScreen: UIViewController {
     
+    @IBOutlet var first: UITextField!
+    
+    @IBOutlet var last: UITextField!
+    
     
     @IBOutlet var usernameField: UITextField!
     
@@ -32,6 +36,45 @@ class RegisterScreen: UIViewController {
         
     }
     
+    func postToServerFunction(){
+        //Contingency Handling. Error handling etc.
+        var firstName : String = first.text!
+        var lastName : String = last.text!
+        var username : String = usernameField.text!
+        var password : String = passwordField.text!
+        var confirmPassword : String = confirmPasswordField.text!
+        var email : String = emailField.text!
+        var phoneNumber : String = phoneField.text!
+        
+        
+        var request = URLRequest(url: URL(string: "http://celebritiessquared.com/CSPhp/Register.php")!)
+        
+        request.httpMethod = "POST"
+        
+        print(password)
+        
+        var postString: String!
+        postString = "first=\(firstName)&last=\(lastName)&username=\(username)&password=\(password)&email=\(email)"
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            
+        }
+        task.resume()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -40,7 +83,14 @@ class RegisterScreen: UIViewController {
     @IBAction func registerButton(_ sender: Any) {
         //send shit to database here
         //change segue to programattically seque if registration passes
+        
+        postToServerFunction()
+        
+        
+        
     }
+    
+    
     
     
     
